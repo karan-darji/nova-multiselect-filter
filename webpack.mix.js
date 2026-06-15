f24dbca8b26f64da830c27b3f85303226152e66d
faijul-ast <faijul.madhi@arsenaltech.com>
2026-06-15 18:46:48 +0530

    build: upgrade dependencies and configure build for Nova 5 migration
    - Add @inertiajs/vue3 to dependencies
    - Upgrade Vue and move it to devDependencies
    - Upgrade build tools including webpack, laravel-mix, sass, and postcss
    - Clean up nova.mix.js by removing resolve-url-loader
    - Remove unnecessary aliases from webpack.mix.js
    - Update compiled assets in dist/js/entry.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,14 +7,4 @@ mix
   .js('resources/js/entry.js', 'js')
   .vue({ version: 3 })
   .css('resources/css/entry.css', 'css')
-  .nova('outl1ne/nova-multiselect-filter')
-  .alias({
-    '@': 'vendor/laravel/nova/resources/js/',
-  })
-  .webpackConfig({
-    resolve: {
-      alias: {
-        'laravel-nova': path.resolve(__dirname, './node_modules/laravel-nova/dist/index.js'),
-      },
-    },
-  });
+  .nova('outl1ne/nova-multiselect-filter');
